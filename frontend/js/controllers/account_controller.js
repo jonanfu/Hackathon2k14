88f81e91ba06ff1284e8a4895d795280f9a101c3
Santi <sa_vi_ag@hotmail.com>
2014-12-30 10:44:01 +0100

    Cambios en el Login
    Tarea del Login realizada.

--- frontend/js/controllers/account_controller.js
+++ frontend/js/controllers/account_controller.js
@@ -9,6 +9,27 @@ define([
 
         AccountController.prototype.login = function(args) {
             loginView.render(args, function(){
+                $('#email').focus();
+                $(document).keypress(function(event) {
+                    var keycode = (event.keyCode ? event.keyCode : event.which);
+                    if(keycode == '13') {
+                        var model = {
+                            email : $('#email').val(),
+                            password : $('#password').val()
+                        };
+                        accountModel.post(model, function(data) {
+                            if (data.isValid === true) {
+                                $('#logout-btn').fadeIn().removeClass('hide');
+                                $('#login-btn').addClass('hide');
+                                window.location.hash = "home/index";
+                            }
+                            else
+                            {
+                                $('#login_error').fadeIn().removeClass('hide');
+                            }
+                        });
+                    }
+                });
                 $('#login-submit').on('click', function(){
                     var model = {
                         email : $('#email').val(),
